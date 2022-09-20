3ac575861691861ead2ec9fb5dd6519a058bbe2d
katiehan22 <katiehan22@gmail.com>
2022-09-20 16:30:36 -0700

    Add views for band controller actions

--- musicapp/app/controllers/bands_controller.rb
+++ musicapp/app/controllers/bands_controller.rb
@@ -2,7 +2,7 @@ class BandsController < ApplicationController
 
   def index
     @bands = Band.all 
-    render :index 
+    render :index # Shows all bands
   end
 
   def create 
@@ -10,18 +10,19 @@ class BandsController < ApplicationController
     if @band.save 
       redirect_to band_url(@band) 
     else 
-      render :new
+      flash.now[:errors] = @band.errors.full_messages
+      render :new 
     end
   end
 
   def new
     @band = Band.new
-    render :new
+    render :new # Form to create new band
   end
 
   def edit
     @band = Band.find_by(id: params[:id])
-    render :edit 
+    render :edit # Form to edit band
   end
 
   def update
@@ -30,13 +31,14 @@ class BandsController < ApplicationController
     if @band.update(band_params)
       redirect_to band_url(@band)
     else
+      flash.now[:errors] = @band.errors.full_messages
       render :edit
     end
   end
 
   def show
     @band = Band.find_by(id: params[:id])
-    render :show
+    render :show # Show one band
   end
 
   def destroy
