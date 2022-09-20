99331612f0a10481cc802632756d38bea0f3c3b6
katiehan22 <katiehan22@gmail.com>
2022-09-20 11:10:45 -0700

    Add SPIRE methods to user model

--- musicapp/app/models/user.rb
+++ musicapp/app/models/user.rb
@@ -14,12 +14,33 @@ class User < ApplicationRecord
   validates :password_digest, presence: true 
   validates :password, length: { minimum: 6 }, allow_nil: true
 
+  before_validation :ensure_session_token
+
   attr_reader :password 
 
   def reset_session_token!
     self.session_token = generate_unique_session_token
     self.save!
     self.session_token
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.is_password?(password)
+      user
+    else
+      nil 
+    end
   end
 
   private
@@ -34,3 +55,4 @@ class User < ApplicationRecord
     self.session_token ||= generate_unique_session_token
   end
 end
+
